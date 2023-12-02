d842871aad2775a533fb4092c45a8b5952c79033
Seth <seth.h.catts@gmail.com>
2023-12-01 21:34:19 -0500

    Solving part 2

--- 2023/Day 1/solution_2.py
+++ 2023/Day 1/solution_2.py
@@ -11,10 +11,24 @@ b = "0"
 total = 0
 firstInt = True
 
-#Look at all lines
-for line in lines:
+for lineNumber in range(0, len(lines)):
+
+    #Ugly but it works!
+    og = lines[lineNumber]
+    lines[lineNumber] = str(lines[lineNumber]).replace("one", "one1one")
+    lines[lineNumber] = str(lines[lineNumber]).replace("two", "two2two")
+    lines[lineNumber] = str(lines[lineNumber]).replace("three", "three3three")
+    lines[lineNumber] = str(lines[lineNumber]).replace("four", "four4four")
+    lines[lineNumber] = str(lines[lineNumber]).replace("five", "five5five")
+    lines[lineNumber] = str(lines[lineNumber]).replace("six", "six6six")
+    lines[lineNumber] = str(lines[lineNumber]).replace("seven", "seven7seven")
+    lines[lineNumber] = str(lines[lineNumber]).replace("eight", "eight8eight")
+    lines[lineNumber] = str(lines[lineNumber]).replace("nine", "nine9nine")
+    print("{} → {}".format(og, lines[lineNumber]))
+
+
     #Look at all the characters in the line 
-    for char in line:
+    for char in lines[lineNumber]:
         if(char.isnumeric() and firstInt):
             a = char
             b = char
@@ -23,7 +37,10 @@ for line in lines:
             b = char
     addition = int(str(a) + str(b))
     total += addition
+    print("A = {}, B = {} | New Total: {}\n\n".format(a, b, total))
     firstInt = True
 
 print("The total is: {}".format(total))
-# Correct answer: 54561
+# Correct answer (p1): 54561
+# Incorrect answer (p2): 53616
+# Correct answer (p2): 54076
