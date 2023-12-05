73024caa08502118b79dfcb833e6c4e2b46d274f
Seth <seth.h.catts@gmail.com>
2023-12-04 19:33:23 -0500

    constitution

--- 2023/Day 3/solution_1.py
+++ 2023/Day 3/solution_1.py
@@ -35,28 +35,54 @@ def hasAdjacentSpecialChar(array, i, j):
     else:
         return False 
 
+def notValidIndex(array, i):
+    if(0 < i < len(array)):
+        return False
+    else:
+        return True
+
 # Edge case: The number is right at the end of the line
 def getWholeNumber(i, line):
     startIndex = i
     endIndex = i
     for l in reversed(range(i)):
-        if(not line[l].isdigit()):
+        if(notValidIndex(line, i)):
+            startIndex = 0
+            break
+        elif(not line[l].isdigit()):
             startIndex = l+1
+            break
+        print("Looked at {}".format(line[l]))
     for l in range(i, len(line)):
-        if(not line[l].isdigit()):
-            endIndex = l-1
-    return [int(line[startIndex:endIndex]), endIndex]
+        if(notValidIndex(line, i)):
+            endIndex = len(line)
+            break
+        elif(not line[l].isdigit()):
+            endIndex = l
+            break
+        print("Looked at {}".format(line[l]))
+    print("Returning: {} | {}".format(line[startIndex:endIndex], endIndex))
+    return [line[startIndex:endIndex], endIndex]
+
+arr = "...............584545456656......"
+for i in range(len(arr)):
+    if(arr[i].isdigit()):
+            numData = getWholeNumber(i, arr[i])
+
+
 
 total = 0
 
-# Do the stuff
-for i in range(0, len(lines)): 
-    for j in range(0, len(lines[i])):
-        if(hasAdjacentSpecialChar(lines, i, j)):
-            #Add this number and move the pointer to the char after the end of this number
-            numData = getWholeNumber(i, lines[i])
-            total += numData[0]
-            i = numData[1]
+# # Do the stuff
+# for i in range(0, len(lines)): 
+#     for j in range(0, len(lines[i])):
+#         print("{}".format(lines[i][j]))
+#         if(hasAdjacentSpecialChar(lines, i, j) and lines[i][j].isdigit()):
+#             #Add this number and move the pointer to the char after the end of this number
+#             numData = getWholeNumber(i, lines[i])
+#             print("Found a valid number | {}".format(numData[0]))
+#             total += int(numData[0])
+#             i = numData[1]
 
 
 print("Total: {}".format(total))
