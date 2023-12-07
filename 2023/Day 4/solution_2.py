6d7ea93ee70c8d897af6a39ec9556a12531a128e
Seth <seth.h.catts@gmail.com>
2023-12-06 19:43:47 -0500

    .

--- 2023/Day 4/solution_2.py
+++ 2023/Day 4/solution_2.py
@@ -2,11 +2,12 @@ input = open("testdata.txt", "r")
 lines = input.readlines()
 total = 0
 
+print(len(lines))
+
 # Loop through all the cards
 # For each winning number on any card
-# gain copies of scratch cards further down the list equal to the number of winning numbers
+# copy a row further down the 
 # 
-
 
 def getCardValue(card):
     card = card.strip()
@@ -20,12 +21,33 @@ def getCardValue(card):
                 value = 1
             else:
                 value *= 2
-    print("Winning numbers: {}\nCard numbers: {}\nValue: {}".format(winningNumbers, cardNumbers, value))
+    #print("Winning numbers: {}\nCard numbers: {}\nValue: {}".format(winningNumbers, cardNumbers, value))
     return value
 
+def insertCards(array, i, value):
+    i += 1
+    insertIndex = i + value + 1
+    newArray = lines.copy()
+    print(newArray)
+    for idx in range(value):
+        newArray.insert(insertIndex, lines[value+idx-1])
+    print("{} \n\n ↓ \n\n {}".format(lines, newArray))
+    return newArray
 
-for line in lines:
-    total += getCardValue(line)
+value = getCardValue(lines[0])
+total += getCardValue(lines[0])
+lines = insertCards(lines, 0, value)
+
+idx = 0
+# while (idx < len(lines)):
+#     value = getCardValue(lines[idx])
+#     total += getCardValue(lines[idx])
+#     lines = insertCards(lines, idx, value)
+#     idx += 1
+
+
+print(idx)
+
 print("Total: {}".format(total))
 
 
